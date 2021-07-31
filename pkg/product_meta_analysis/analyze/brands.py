32b063151a60f45a1c9b41f84740dee35c8cdf82
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-07-31 18:00:48 -0400

    simple bramd recognition functions

---
+++ pkg/product_meta_analysis/analyze/brands.py
@@ -0,0 +1,48 @@
+import operator
+
+from nltk import ngrams
+
+class BrandRecognizerBase:
+    def __init__(self, brands):
+        self.brands = brands
+
+    def get_brands(self, sentences):
+        brands = [
+            strip_nulls([self._get_brand(token) for token in sentence])
+            for sentence in sentences
+            ]
+        return brands
+
+class FuzzyBrandRecognizer(BrandRecognizerBase):
+    def __init__(self, brands):
+        super().__init__(brands=brands)
+        self.n_grams = 3
+        self.brands_ngrams = {x:get_ngram_strings(x, self.n_grams) for x in self.brands}
+        self.min_token_length = 5
+        self.jaccard_threshold = 0.25
+
+    def _get_brand(self, token:str):
+        n_char = token.end_char - token.start_char
+        if n_char >= self.min_token_length:
+            token_ngrams = get_ngram_strings(token.text, self.n_grams)
+            scores = {
+                k:get_jaccard_dist(token_ngrams, v)
+                for k, v in self.brands_ngrams.items()
+                }
+            best_match, best_score = max(
+                scores.items(),
+                key=operator.itemgetter(1)
+                )
+            if best_score > self.jaccard_threshold:
+                return best_match
+
+def get_ngram_strings(s, n):
+    return [''.join(x).lower() for x in ngrams(s, n)]
+
+def get_jaccard_dist(list1, list2):
+    s1 = set(list1)
+    s2 = set(list2)
+    return float(len(s1.intersection(s2))) / float(len(s1.union(s2)))
+
+def strip_nulls(l):
+    return [x for x in set(l) if x is not None]
--- scripts/parse_reddit_comments.py
+++ scripts/parse_reddit_comments.py
@@ -1,15 +1,24 @@
+import pandas as pd
+
 from product_meta_analysis.database import Database
 from product_meta_analysis.analyze import tokenize
+from product_meta_analysis.analyze import brands
+from product_meta_analysis.utils import read_config
 
+# TODO: Long term we should identiy brands based on text
+file_name = 'gluten_free_flour'
+
+# get comments
 db = Database()
 query = f"""
-    select body
+    select body, id
     from reddit_comments
     where category = 'gluten_free_flour'
     limit 20
     """
 comments = db.read(query)
 
+# tokenize comments
 def get_noun_phrases(comments):
     s = tokenize.Tokenizer()
     comments = [
@@ -17,5 +26,60 @@ def get_noun_phrases(comments):
         for x in comments
         ]
     return comments
+tokens = get_noun_phrases(comments)
 
-print(get_noun_phrases(comments))
+# identify brands
+def get_brands(tokens, brand_names):
+    fbr = brands.FuzzyBrandRecognizer(brands=brand_names)
+    brands_ = [fbr.get_brands(x) for x in tokens]
+    return brands_
+brand_names = read_config('reddit_comments', file_name).get('brands')
+brands_ = get_brands(tokens, brand_names)
+
+# format data
+def format_data(comments, brands_, brand_names):
+    ids = [x[1] for x in comments]
+    data = [
+        [id, ix, sentence]
+        for id, sentences
+        in zip(ids, brands_)
+        for ix, sentence in enumerate(sentences)
+        ]
+    data = pd.DataFrame(
+        data,
+        columns=['id', 'sentence_ix', 'brands']
+        )
+    data['sentiment'] = 'None'
+    data = data.explode('brands')
+    brand_names_ = {x:ix for ix, x in enumerate(brand_names)}
+    data['brand_ix'] = data['brands'].map(brand_names_)
+    data['annotation_id'] = data['id'] + data['sentence_ix'].astype(str) + data['brand_ix'].fillna(-1).astype(int).astype(str)
+    return data
+data = format_data(comments, brands_, brand_names)
+print(data)
+
+
+# load data
+def load_data(data, db):
+    crate_query = f""" CREATE TABLE IF NOT EXISTS reddit_comment_annotations (
+    	id text,
+        sentence_ix int,
+        brands text,
+    	sentiment text,
+        brand_ix int,
+        annotation_id text PRIMARY KEY,
+        UNIQUE(annotation_id)
+        )"""
+    db.write(crate_query)
+
+    data.to_sql(
+        name='tmp',
+        con=db._con,
+        if_exists = 'append',
+        index=False
+        )
+    db.write('INSERT OR IGNORE INTO reddit_comment_annotations SELECT * FROM tmp')
+    db.drop('tmp')
+
+db = Database()
+load_data(data, db)
