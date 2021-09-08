e81ff44fd6066deac8d368746fe9872f10684066
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-09-07 21:20:21 -0400

    grab individual urls manually for get urls script

--- scripts/website/get_website_urls.py
+++ scripts/website/get_website_urls.py
@@ -1,5 +1,6 @@
 import datetime
 import hashlib
+from urllib.parse import urlparse
 
 import pandas as pd
 
@@ -11,9 +12,12 @@ from product_meta_analysis.utils import read_config
 def hash_url(s):
     return hashlib.sha256(s.encode('utf-8')).hexdigest()
 
-def get_urls(domains):
+def standardize_domain(url):
+    return urlparse(url).netloc
+
+def get_urls_by_domain(domains):
     urls = [
-        [hash_url(url), url, domain]
+        [hash_url(url), url, standardize_domain(domain)]
         for domain in domains
         for url in s.extract_urls_from_sitemap(domain)
         ]
@@ -21,6 +25,21 @@ def get_urls(domains):
     urls['process_datetime'] = datetime.datetime.now()
     urls['process_date'] = datetime.date.today()
     return urls
+
+def get_manual_urls(manual_urls):
+    urls = [
+        [hash_url(url), url, standardize_domain(url)]
+        for url in manual_urls
+        ]
+    urls = pd.DataFrame(urls, columns=['url_id', 'url', 'domain'])
+    urls['process_datetime'] = datetime.datetime.now()
+    urls['process_date'] = datetime.date.today()
+    return urls
+
+def get_urls(domains, manual_urls):
+    d = get_urls_by_domain(domains)
+    u = get_manual_urls(manual_urls)
+    return pd.concat([d,u], axis=0)
 
 def save_urls(data, database):
     data.to_sql(
@@ -36,8 +55,9 @@ config_type = 'website_content'
 config_name = 'example'
 config = read_config(config_type, config_name)
 domains = config.get('urls').get('domains')
+manual_urls = config.get('urls').get('urls')
 
 db = Database()
-urls = get_urls(domains)
+urls = get_urls(domains, manual_urls)
 save_urls(urls, db)
 db.close()
